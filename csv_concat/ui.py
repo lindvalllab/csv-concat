35d42272e422270f0be37e01aa3f6ae861a1cc96
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2022-08-17 15:48:19 -0400

    add loading bar

--- csv_concat/ui.py
+++ csv_concat/ui.py
@@ -1,5 +1,9 @@
+import multiprocessing
+import sys
 import tkinter as tk
-from tkinter import filedialog, ttk
+from tkinter import filedialog, messagebox, ttk
+from typing import Callable
+
 from concatenator import Concatenator
 
 
@@ -81,7 +85,47 @@ class UserInterface:
         output_filename = filedialog.asksaveasfilename(
             filetypes=[("Comma-separated files", "*.csv")]
         )
-        columns = self.columns_box.curselection()
-        if output_filename is not None:
-            with open(output_filename, 'w') as output_file:
-                self.concatenator.write_file(output_file, columns)
+        if output_filename != '' and output_filename is not None:
+            self.columns_box.config(state=tk.DISABLED)
+            self.select_files_button.config(state=tk.DISABLED)
+            self.run_button.config(state=tk.DISABLED)
+
+            def process() -> None:
+                columns = self.columns_box.curselection()
+                with open(output_filename, 'w') as output_file:
+                    self.concatenator.write_file(output_file, columns)
+
+            thread = multiprocessing.Process(target=process)
+            thread.start()
+
+            def on_finish() -> None:
+                messagebox.showinfo('Done!', message='Files combined successfully')
+                self.columns_box.config(state=tk.NORMAL)
+                self.select_files_button.config(state=tk.NORMAL)
+                self.run_button.config(state=tk.NORMAL)
+
+            create_loading_window(self.root, thread, on_finish)
+
+
+def create_loading_window(root: tk.Tk,
+                          thread: multiprocessing.Process,
+                          on_finish: Callable[[], None]) -> None:
+    loading_window = tk.Toplevel(root)
+    if sys.platform.startswith('linux'):
+        loading_window.attributes('-type', 'dialog')
+    elif sys.platform.startswith('windows'):
+        loading_window.attributes('-toolwindow', True)
+    loading_window.title('Processing')
+    progress = ttk.Progressbar(loading_window, orient='horizontal', mode='indeterminate')
+    progress.pack()
+    progress.start(10)
+
+    def check_if_running() -> None:
+        if thread.is_alive():
+            loading_window.after(10, check_if_running)
+        else:
+            loading_window.destroy()
+            on_finish()
+
+    loading_window.after(50, check_if_running)
+    loading_window.mainloop()
