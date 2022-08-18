3b8da78449366734e7c0954dba411cd9223a62dd
Ben FL <benjamin_fayyazuddinljungberg@dfci.harvard.edu>
2022-08-18 16:37:54 -0400

    fix windows multiprocessing

--- csv_concat/csv_concat.py
+++ csv_concat/csv_concat.py
@@ -1,5 +1,11 @@
+import multiprocessing
+
 from ui import UserInterface
 
 if __name__ == '__main__':
+    # Required for building on Windows
+    # https://github.com/pyinstaller/pyinstaller/wiki/Recipe-Multiprocessing
+    multiprocessing.freeze_support()
+
     ui = UserInterface()
     ui.run()
